924c52f6b215aa2aa77322b4e93e984258802369
Mr-Haji <abraheemofficial@gmail.com>
2024-11-21 23:35:32 +0500

    Header And Footer Reaponsivnes pending

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,34 +1,224 @@
-import { Grid2 as Grid } from '@mui/material'
-import React from 'react'
+// import { Grid, Stack } from '@mui/material';
+// import React from 'react';
+// import Logo from "../../assets/Logo.svg";
+
+// const Footer = () => {
+//     return (
+//         <Grid
+//             container
+//             alignItems="center"
+//             spacing={2}
+//             sx={{ width: '100%', bgcolor: '#171921', color: 'white', padding: 3 }}
+//         >
+//             {/* Section 1 */}
+//             <Grid item sx={{ display: { xs: "none", md: 'block' } }} md={3}>
+
+//                 <img src={Logo} alt="Logo" style={{ width: '150px', }} />
+
+//             </Grid>
+
+//             {/* Section 2 */}
+//             <Grid item xs={6} md={3}>
+//                 <h3 style={{ marginBottom: '10px' }}>Products & Technology</h3>
+//                 <ul style={{ listStyleType: 'none', padding: 0, color: "#87878c" }}>
+//                     <li style={{ marginBottom: "8px" }}>Smart Contract Audit</li>
+//                     <li style={{ marginBottom: "8px" }}>Skynet</li>
+//                     <li style={{ marginBottom: "8px" }}>Security Score</li>
+//                     <li style={{ marginBottom: "8px" }}>   KYC</li>
+//                     <li style={{ marginBottom: "8px" }}>   Penetration Testing</li>
+//                     <li style={{ marginBottom: "8px" }}>   Bug Bounty</li>
+//                     <li style={{ marginBottom: "8px" }}>   Formal Verification</li>
+//                     <li style={{ marginBottom: "8px" }}>   Advisory Services</li>
+//                     <li style={{ marginBottom: "8px" }}>   SkyHarbor</li>
+//                     <li style={{ marginBottom: "8px" }}>   SkyInsights</li>
+//                 </ul>
+//             </Grid>
+//             {/* Section 2 */}
+//             <Grid item xs={6} md={3}>
+//                 <h3 style={{ marginBottom: '10px' }}>Company</h3>
+//                 <ul style={{ listStyleType: 'none', padding: 0, color: "#87878c" }}>
+//                     <li style={{ marginBottom: "8px" }}>About</li>
+//                     <li style={{ marginBottom: "8px" }}>Ventures</li>
+//                     <li style={{ marginBottom: "8px" }}>Blog</li>
+//                     <li style={{ marginBottom: "8px" }}>Careers</li>
+//                     <li style={{ marginBottom: "8px" }}> Disclaimer</li>
+//                     <li style={{ marginBottom: "8px" }}>Privacy Policy</li>
+//                     <li style={{ marginBottom: "8px" }}>Cookie Policy</li>
+//                     <li style={{ marginBottom: "8px" }}>Terms and Conditions</li>
+//                     <li style={{ marginBottom: "8px" }}>Trust and Security</li>
+//                 </ul>
+//             </Grid>
+//             <Grid item xs={12} md={3}>
+//                 <h3 style={{ marginBottom: '10px' }}>Social Media</h3>
+//                 <ul style={{ listStyleType: 'none', padding: 0, color: "#87878c" }}>
+//                     <li style={{ marginBottom: "8px" }}>Social Media</li>
+//                     <li style={{ marginBottom: "8px" }}>TwitterCertiK</li>
+//                     <li style={{ marginBottom: "8px" }}>TwitterSecurity Leaderboard</li>
+//                     <li style={{ marginBottom: "8px" }}>TwitterCertiK Alert</li>
+//                     <li style={{ marginBottom: "8px" }}>TelegramTelegram</li>
+//                     <li style={{ marginBottom: "8px" }}>YoutubeYoutube</li>
+//                     <li style={{ marginBottom: "8px" }}>MediumMedium</li>
+//                     <li style={{ marginBottom: "8px" }}>LinkedinLinkedin</li>
+//                     <li style={{ marginBottom: "8px" }}>WeChatWechat</li>
+//                     <li style={{ marginBottom: "8px" }}>DiscordDiscord</li>
+//                 </ul>
+//             </Grid>
+//             {/* Section 3 */}
+//             <Grid item xs={12} md={4}>
+//                 <p>© 2024 by CertiK. All Rights Reserved.</p>
+//             </Grid>
+//         </Grid>
+//     );
+// };
+
+// export default Footer;
+import { Grid, Stack } from '@mui/material';
+import React from 'react';
+import Logo from "../../assets/Logo.svg";
 
 const Footer = () => {
     return (
-        <Grid container sx={{ bgcolor: 'black' }} width={"100%"}>
 
-            <Grid item sx={{ color: 'white' }} md={2}>
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-            </Grid>
-            <Grid item sx={{ color: 'white' }} md={2}>
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-            </Grid>
-            <Grid item sx={{ color: 'white' }} md={2}>
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-                sadjgkjkjkjjklkljljljlkj
-            </Grid>
-        </Grid>
 
-    )
-}
+        <Stack sx={{ flexDirection: 'row', bgcolor: '#171921', width: '100vw', justifyContent: 'space-around', py: 2, color: 'white' }}>
+            <Stack sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                <Stack>
+                    <img src={Logo} alt="Logo" style={{ width: '150px' }} />
+                </Stack>
+                <Stack><p>© 2024 by CertiK. All Rights Reserved.</p></Stack>
+            </Stack>
+            <Stack>   <h3 style={{ marginBottom: '10px', color: 'white' }}>Products & Technology</h3>
+                <ul style={{ listStyleType: 'none', padding: 0, color: "#87878c" }}>
+                    <li style={{ marginBottom: "8px" }}>Smart Contract Audit</li>
+                    <li style={{ marginBottom: "8px" }}>Skynet</li>
+                    <li style={{ marginBottom: "8px" }}>Security Score</li>
+                    <li style={{ marginBottom: "8px" }}>   KYC</li>
+                    <li style={{ marginBottom: "8px" }}>   Penetration Testing</li>
+                    <li style={{ marginBottom: "8px" }}>   Bug Bounty</li>
+                    <li style={{ marginBottom: "8px" }}>   Formal Verification</li>
+                    <li style={{ marginBottom: "8px" }}>   Advisory Services</li>
+                    <li style={{ marginBottom: "8px" }}>   SkyHarbor</li>
+                    <li style={{ marginBottom: "8px" }}>   SkyInsights</li>
+                </ul></Stack>
+            <Stack>
+                <h3 style={{ marginBottom: '10px', color: 'white' }}>Company</h3>
+                <ul style={{ listStyleType: 'none', padding: 0, color: "#87878c" }}>
+                    <li style={{ marginBottom: "8px" }}>About</li>
+                    <li style={{ marginBottom: "8px" }}>Ventures</li>
+                    <li style={{ marginBottom: "8px" }}>Blog</li>
+                    <li style={{ marginBottom: "8px" }}>Careers</li>
+                    <li style={{ marginBottom: "8px" }}> Disclaimer</li>
+                    <li style={{ marginBottom: "8px" }}>Privacy Policy</li>
+                    <li style={{ marginBottom: "8px" }}>Cookie Policy</li>
+                    <li style={{ marginBottom: "8px" }}>Terms and Conditions</li>
+                    <li style={{ marginBottom: "8px" }}>Trust and Security</li>
+                </ul>
+            </Stack>
+            <Stack>   <h3 style={{ marginBottom: '10px', color: 'white' }}>Social Media</h3>
+                <ul style={{ listStyleType: 'none', padding: 0, color: "#87878c" }}>
+                    <li style={{ marginBottom: "8px" }}>Social Media</li>
+                    <li style={{ marginBottom: "8px" }}>TwitterCertiK</li>
+                    <li style={{ marginBottom: "8px" }}>TwitterSecurity Leaderboard</li>
+                    <li style={{ marginBottom: "8px" }}>TwitterCertiK Alert</li>
+                    <li style={{ marginBottom: "8px" }}>TelegramTelegram</li>
+                    <li style={{ marginBottom: "8px" }}>YoutubeYoutube</li>
+                    <li style={{ marginBottom: "8px" }}>MediumMedium</li>
+                    <li style={{ marginBottom: "8px" }}>LinkedinLinkedin</li>
+                    <li style={{ marginBottom: "8px" }}>WeChatWechat</li>
+                    <li style={{ marginBottom: "8px" }}>DiscordDiscord</li>
+                </ul></Stack>
+        </Stack>
 
-export default Footer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // <Grid
+        //     container
+        //     alignItems="center"
+        //     spacing={2}
+        //     sx={{ width: '100%', bgcolor: '#171921', color: 'white', padding: 3 }}
+        // >
+        //     {/* Section 1
+        //     <Grid item sx={{ display: { xs: "none", md: 'block' } }} md={3}>
+
+        //         <img src={Logo} alt="Logo" style={{ width: '150px', }} />
+
+        //     </Grid>
+
+        //     {/* Section 2 */}
+        //     <Grid item xs={6} md={3}>
+        //         <h3 style={{ marginBottom: '10px' }}>Products & Technology</h3>
+        //         <ul style={{ listStyleType: 'none', padding: 0, color: "#87878c" }}>
+        //             <li style={{ marginBottom: "8px" }}>Smart Contract Audit</li>
+        //             <li style={{ marginBottom: "8px" }}>Skynet</li>
+        //             <li style={{ marginBottom: "8px" }}>Security Score</li>
+        //             <li style={{ marginBottom: "8px" }}>   KYC</li>
+        //             <li style={{ marginBottom: "8px" }}>   Penetration Testing</li>
+        //             <li style={{ marginBottom: "8px" }}>   Bug Bounty</li>
+        //             <li style={{ marginBottom: "8px" }}>   Formal Verification</li>
+        //             <li style={{ marginBottom: "8px" }}>   Advisory Services</li>
+        //             <li style={{ marginBottom: "8px" }}>   SkyHarbor</li>
+        //             <li style={{ marginBottom: "8px" }}>   SkyInsights</li>
+        //         </ul>
+        //     </Grid> */}
+        //     {/* Section 2 */}
+        //     <Grid item xs={6} md={3}>
+        //         <h3 style={{ marginBottom: '10px' }}>Company</h3>
+        //         <ul style={{ listStyleType: 'none', padding: 0, color: "#87878c" }}>
+        //             <li style={{ marginBottom: "8px" }}>About</li>
+        //             <li style={{ marginBottom: "8px" }}>Ventures</li>
+        //             <li style={{ marginBottom: "8px" }}>Blog</li>
+        //             <li style={{ marginBottom: "8px" }}>Careers</li>
+        //             <li style={{ marginBottom: "8px" }}> Disclaimer</li>
+        //             <li style={{ marginBottom: "8px" }}>Privacy Policy</li>
+        //             <li style={{ marginBottom: "8px" }}>Cookie Policy</li>
+        //             <li style={{ marginBottom: "8px" }}>Terms and Conditions</li>
+        //             <li style={{ marginBottom: "8px" }}>Trust and Security</li>
+        //         </ul>
+        //     </Grid>
+        //     <Grid item xs={12} md={3}>
+        //         <h3 style={{ marginBottom: '10px' }}>Social Media</h3>
+        //         <ul style={{ listStyleType: 'none', padding: 0, color: "#87878c" }}>
+        //             <li style={{ marginBottom: "8px" }}>Social Media</li>
+        //             <li style={{ marginBottom: "8px" }}>TwitterCertiK</li>
+        //             <li style={{ marginBottom: "8px" }}>TwitterSecurity Leaderboard</li>
+        //             <li style={{ marginBottom: "8px" }}>TwitterCertiK Alert</li>
+        //             <li style={{ marginBottom: "8px" }}>TelegramTelegram</li>
+        //             <li style={{ marginBottom: "8px" }}>YoutubeYoutube</li>
+        //             <li style={{ marginBottom: "8px" }}>MediumMedium</li>
+        //             <li style={{ marginBottom: "8px" }}>LinkedinLinkedin</li>
+        //             <li style={{ marginBottom: "8px" }}>WeChatWechat</li>
+        //             <li style={{ marginBottom: "8px" }}>DiscordDiscord</li>
+        //         </ul>
+        //     </Grid>
+        //     {/* Section 3 */}
+        //     <Grid item xs={12} md={4}>
+        //         <p>© 2024 by CertiK. All Rights Reserved.</p>
+        //     </Grid>
+        // </Grid>
+    );
+};
+
+export default Footer;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -227,9 +227,9 @@ export default function Header() {
     return (
         <AppBar position="static" sx={{ bgcolor: 'black' }}>
             <Toolbar>
-                <Grid container alignItems="center"  spacing={2} sx={{ width: '100%' }}>
+                <Grid container alignItems="center" spacing={2} sx={{ width: '100%' }}>
                     {/* Logo and Dropdowns */}
-                    <Grid item xs={12} sm={4} md={4} container alignItems="center">
+                    <Grid item xs={6} sm={4} md={4} container alignItems="center">
                         <img src={Logo} alt="Logo" style={{ width: '150px', marginRight: '10px' }} />
                         <Typography variant="h6" sx={{ fontSize: '16px', fontWeight: 'bold', display: { xs: 'none', sm: 'block' } }}>
                             Products <ArrowDropDown />
@@ -240,45 +240,45 @@ export default function Header() {
                     </Grid>
 
                     {/* Search Bar */}
-                    <Grid item xs={12} sm={4} md={4}>
-                    <Box
-    sx={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: 1,
-        bgcolor: '#45474d',
-        borderRadius: '10px',
-        px: 2,
-        width: '100%',
-        maxWidth: '600px',
-        height: '40px', // Ensures consistent height
-    }}
->
-    <SearchIcon sx={{ fontSize: '30px', color: 'white' }} />
-    <StyledInputBase
- placeholder="Search Project"
- sx={{
-     flex: 1,
-     color: 'white',
-     fontSize: '16px',
-     '& .MuiInputBase-input': {
-         padding: 0,
-         height: '100%',
-     },
-     '&:focus': {
-         outline: 'none', // Removes the default focus outline
-     },
- }}
- inputProps={{
-     style: {
-         outline: 'none', // Ensures no outline on focus
-     },
- }}
-    />
-</Box>
+                    <Grid item sx={{ display: { xs: "none", md: 'block' } }} sm={4} md={4}>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                gap: 1,
+                                bgcolor: '#45474d',
+                                borderRadius: '10px',
+                                px: 2,
+                                width: '100%',
+                                maxWidth: '600px',
+                                height: '40px', // Ensures consistent height
+                            }}
+                        >
+                            <SearchIcon sx={{ fontSize: '30px', color: 'white' }} />
+                            <StyledInputBase
+                                placeholder="Search Project"
+                                sx={{
+                                    flex: 1,
+                                    color: 'white',
+                                    fontSize: '16px',
+                                    '& .MuiInputBase-input': {
+                                        padding: 0,
+                                        height: '100%',
+                                    },
+                                    '&:focus': {
+                                        outline: 'none', // Removes the default focus outline
+                                    },
+                                }}
+                                inputProps={{
+                                    style: {
+                                        outline: 'none', // Ensures no outline on focus
+                                    },
+                                }}
+                            />
+                        </Box>
 
-                        
-                        
+
+
                         {/* <Box sx={{ display: 'flex', alignItems: 'center', gap: 1,  }}>
                             <SearchIcon sx={{ fontSize: '30px', color: 'white' ,}} />
                             <StyledInputBase sx={{height:"40px"}}
@@ -291,12 +291,12 @@ export default function Header() {
                     </Grid>
 
                     {/* Actions */}
-                    <Grid item xs={12} sm={4} md={4} container alignItems="center" justifyContent="flex-end" spacing={1}>
-                        <Grid item>
+                    <Grid item xs={5} sm={4} md={4} container alignItems="center" justifyContent="flex-end" spacing={1}>
+                        <Grid sx={{ display: { xs: "none",md:'block' } }} item>
                             <LanguageOutlined sx={{ fontSize: '30px', color: 'white' }} />
                             <img src={ResponseIcon} alt="Response Icon" style={{ width: '30px' }} />
                         </Grid>
-                        <Grid item>
+                        <Grid sx={{ display: { xs: "none",md:'block' } }} item>
                             <Button sx={{ color: 'white' }}>Log In</Button>
                             <Button sx={{ color: 'white' }}>Sign Up</Button>
                         </Grid>
@@ -307,9 +307,11 @@ export default function Header() {
                                     bgcolor: '#e5453d',
                                     color: 'white',
                                     px: 3,
+                                    my: 1,
+
                                     fontWeight: 'bold',
                                     borderRadius: '10px',
-                                    height: '40px',
+                                    // height: '40px',
                                     textTransform: 'none',
                                 }}
                             >
